e2644b8022e03c89271e38116a2c62362ca7824b
Krishna Singh Mahar <krishnasinghmaharksm@gmail.com>
2024-06-19 20:21:55 +0530

    type of conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "33";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33ab" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 2
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn) 
+
+// 1 => true; 0 => false
+// "" => false
+// "krishna" => true
+// 2 => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
