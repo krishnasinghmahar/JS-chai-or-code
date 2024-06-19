3579952a055f1ad93079fa131b36cdd244bf6552
Krishna Singh Mahar <krishnasinghmaharksm@gmail.com>
2024-06-19 21:52:55 +0530

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,4 +21,39 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+// console.log(typeof stringNumber)
+
+// ********* Operations ************
+
+let value = 3
+let negValue = -value
+// console.log(negValue) // -3
+
+// console.log(2 + 3)
+// console.log(2 - 3)
+// console.log(2 * 3)
+// console.log(2 ** 3)
+// console.log(2 / 3)
+// console.log(2 % 3)
+
+let str1 = "Hello"
+let str2 = " World"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 3)
+// console.log(1 + 2 + "3")
+
+// console.log((1 + 2) / 3 % 4); // use brackets
+
+// console.log(+true) // 1
+// console.log(+"") // 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 4
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
