3a3ecf0dc9266ac3efa835a8ee3170e3d2265786
Krishna Singh Mahar <krishnasinghmaharksm@gmail.com>
2024-06-19 19:52:19 +0530

    variables and dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 12345
+let accountEmail = 'krishna@gmail.com'
+var accountPassword = '12345'
+accountCity = 'Jaipur'
+let accountState
+
+// accountId = 2 not allowed
+
+accountEmail = 'k@gmail.com'
+accountPassword = '******'
+accountCity = 'Chennai'
+
+console.log(accountId)
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all JS code as newer version 
+
+// alert(3 + 3) we are using nodejs, not browser
+
+console.log(3 + 3) // code readability should be high
+
+let name = "krishna"
+let age = 21
+let isLoggedIn = false
+
+// primitives
+
+// number => 2 to the power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => let state;
+// symbol => unique
+
+
+// object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
