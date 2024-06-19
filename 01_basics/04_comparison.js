d9256905b47c16823979c38cb827da3ad6e4d86b
Krishna Singh Mahar <krishnasinghmaharksm@gmail.com>
2024-06-19 22:10:36 +0530

    Comparison Operators

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -5,3 +5,17 @@
 // console.log(2 == 1)
 // console.log(2 != 1)
 
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0); false
+// console.log(null == 0); false
+// console.log(null >= 0); true
+
+// console.log(undefined > 0); false
+// console.log(undefined == 0); false
+// console.log(undefined >= 0); false
+
+// === strict check
+
+console.log("2" === 2) // false
