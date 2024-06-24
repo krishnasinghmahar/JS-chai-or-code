01758aeb6b5a453ede15413a58f70ec9955ef938
Krishna Singh Mahar <krishnasinghmaharksm@gmail.com>
2024-06-24 18:26:40 +0530

    refactor code use switch case instead of if else

--- 07_projects/01_colorChanger/chaiaurcode.js
+++ 07_projects/01_colorChanger/chaiaurcode.js
@@ -30,17 +30,33 @@ const body = document.querySelector('body')
 
 buttons.forEach((button) => {
   button.addEventListener('click', (e) => {
-    if (e.target.id === 'grey') {
-      body.style.backgroundColor = e.target.id
-    }
-    if (e.target.id === 'white') {
-      body.style.backgroundColor = e.target.id
-    }
-    if (e.target.id === 'blue') {
-      body.style.backgroundColor = e.target.id
-    }
-    if (e.target.id === 'yellow') {
-      body.style.backgroundColor = e.target.id
+    // if (e.target.id === 'grey') {
+    //   body.style.backgroundColor = e.target.id
+    // }
+    // if (e.target.id === 'white') {
+    //   body.style.backgroundColor = e.target.id
+    // }
+    // if (e.target.id === 'blue') {
+    //   body.style.backgroundColor = e.target.id
+    // }
+    // if (e.target.id === 'yellow') {
+    //   body.style.backgroundColor = e.target.id
+    // }
+    switch (e.target.id) {
+      case 'grey':
+        body.style.backgroundColor = e.target.id
+        break;
+      case 'white':
+        body.style.backgroundColor = e.target.id
+        break;
+      case 'blue':
+        body.style.backgroundColor = e.target.id
+        break;
+      case 'yellow':
+        body.style.backgroundColor = e.target.id
+        break;
+      default:
+        break;
     }
   })
 })
